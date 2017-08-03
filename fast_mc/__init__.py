77f713e8b4a33aaa658bf35598f76e69581158c0
Sreekumar Thaithara Balan <tbs1980@gmail.com>
2017-08-03 11:52:08 +0100

    added author info in setup. version 0.0.1 created. init modifed with included packages

---
+++ fast_mc/__init__.py
@@ -0,0 +1,6 @@
+from fast_mc.version import __version__
+from fast_mc.model import Model
+from fast_mc.ensemble import EnsembleSampler
+from fast_mc.hmc import HamiltonianMonteCarloSampler
+from fast_mc.rmhmc import RiemannianManifoldHamiltonianSampler
+
--- setup.py
+++ setup.py
@@ -1,7 +1,12 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(
     name='fast_mc',
+    description='A collection of Monte Carlo Samplers',
+    author='Sreekumar Thaithara Balan',
+    author_email='tbs1980@gmail.com',
+    version='0.0.1',
+    packages=find_packages(exclude=['doc', 'tests*']),
     install_requires=[
         'numpy'
     ],
